177ad9ebf03d43d5a86d5058b81beccd7cd8f791
Reto Kleeb <retokl@gmail.com>
2011-05-16 13:52:49 -0400

    minor change (class should be obsolete after changes to GetUsertalkNetwork)

--- src/main/java/edu/mit/cci/visualize/wiki/collector/Result.java
+++ src/main/java/edu/mit/cci/visualize/wiki/collector/Result.java
@@ -2,59 +2,59 @@ package edu.mit.cci.visualize.wiki.collector;
 
 public class Result {
 
-	private String data;
-	private int count;
-	private String nextId;
-	private boolean archive;
+    private String data;
+    private int count;
+    private String nextId;
+    private boolean archive;
 
-	public Result() {
-		data = new String();
-		count = 0;
-		nextId = "0";
-		archive = false;
-	}
+    public Result() {
+        data = new String();
+        count = 0;
+        nextId = "0";
+        archive = false;
+    }
 
-	public void setResult(final String _data) {
-		data = _data;
-	}
+    public void setResult(final String _data) {
+        data = _data;
+    }
 
-	public void append(final String _data) {
-		data += _data;
-		count++;
-	}
+    public void append(final String _data) {
+        data += _data;
+        count++;
+    }
 
-	public String getResult() {
-		return data;
-	}
+    public String getResult() {
+        return data;
+    }
 
-	public int getCount() {
-		return count;
-	}
+    public int getCount() {
+        return count;
+    }
 
-	public void clear() {
-		data = "";
-		count = 0;
-		nextId = "0";
-		archive = false;
-	}
+    public void clear() {
+        data = "";
+        count = 0;
+        nextId = "0";
+        archive = false;
+    }
 
-	public void setNextId(final String _nextId) {
-		nextId = _nextId;
-	}
+    public void setNextId(final String _nextId) {
+        nextId = _nextId;
+    }
 
-	public String getNextId() {
-		return nextId;
-	}
+    public String getNextId() {
+        return nextId;
+    }
 
-	public boolean hasNextId() {
-		return !"0".equals(nextId);
-	}
+    public boolean hasNextId() {
+        return !"0".equals(nextId);
+    }
 
-	public boolean getArchive() {
-		return archive;
-	}
+    public boolean getArchive() {
+        return archive;
+    }
 
-	public void setArchive(final boolean flag) {
-		archive = flag;
-	}
+    public void setArchive(final boolean flag) {
+        archive = flag;
+    }
 }
