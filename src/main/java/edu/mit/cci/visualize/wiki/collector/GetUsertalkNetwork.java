3318380fc422247696834251d3c7de7341ac7d62
Reto Kleeb <retokl@gmail.com>
2011-05-17 10:08:48 -0400

    simplyfied code for user talk matrix creation

--- src/main/java/edu/mit/cci/visualize/wiki/collector/GetUsertalkNetwork.java
+++ src/main/java/edu/mit/cci/visualize/wiki/collector/GetUsertalkNetwork.java
@@ -1,106 +1,81 @@
 package edu.mit.cci.visualize.wiki.collector;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.logging.Logger;
 
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.mit.cci.visualize.wiki.util.Const;
+import edu.mit.cci.visualize.wiki.util.WikiAPIClient;
+import edu.mit.cci.visualize.wiki.xml.Api;
+import edu.mit.cci.visualize.wiki.xml.XMLTransformer;
 
 public class GetUsertalkNetwork {
 
-	private static final Logger LOG = Logger.getLogger(GetUsertalkNetwork.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(GetUsertalkNetwork.class.getName());
 
-	public String getNetwork(final String lang, final String nodes) {
-		String data = "";
+    public String getNetwork(final String lang, final String nodes) {
+        String data = "";
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        WikiAPIClient wikiAPIClient = new WikiAPIClient(httpclient, false);
+        try {
+            String[] node = nodes.split("\n");
+            int[][] userTalkMatrix = new int[node.length][node.length];
 
-		Result result = new Result();
-		try {
-			String[] node = nodes.split("\n");
-			int[][] matrix = new int[node.length][node.length];
-			for (int i = 0; i < node.length; i++) {
-				for (int j = 0; j < node.length; j++) {
-					if (i == j) {
-						continue;
-					}
-					String from = node[i].split("\t")[0];
-					from = from.replaceAll(" ","_");
-					String to = node[j].split("\t")[0];
-					to = to.replaceAll(" ", "_");
+            for (int i = 0; i < node.length; i++) {
+                for (int j = 0; j < node.length; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+                    String from = node[i].split("\t")[0];
+                    from = from.replaceAll(" ", "_");
+                    String to = node[j].split("\t")[0];
+                    to = to.replaceAll(" ", "_");
 
-					String xml = getUserTalkContribs(lang, to, from, "");
-					if (xml.indexOf("<revisions>") > 0) {
-						LOG.info(xml);
-						XMLParseUserTalk parse = new XMLParseUserTalk(to, result, xml);
-						parse.parse();
-					}
-					String out = result.getResult();
+                    String xml = getUserTalkContribs(lang, to, from, wikiAPIClient);
+                    if (xml.indexOf("<revisions>") > 0) {
+                        Api revisionFromXML = XMLTransformer.getRevisionFromXML(xml);
+                        int numberOfRevisions = revisionFromXML.getAllRevisionsForRequest().size();
+                        if (numberOfRevisions >= 1) {
+                            userTalkMatrix[i][j] = numberOfRevisions;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < userTalkMatrix.length; i++) {
+                for (int j = i + 1; j < userTalkMatrix[i].length; j++) {
+                    int value = userTalkMatrix[i][j] + userTalkMatrix[j][i]; //sum of talk in both directions
+                    if (value > 0) {
+                        data += node[i].split("\t")[0] + "\t" + node[j].split("\t")[0] + "\t" + value + "\n";
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Exception while fetching UserTalk Info", e);
+        } finally {
+            httpclient.getConnectionManager().shutdown();
+        }
+        return data;
+    }
 
-					//log.info(i + "-" + from + " " + j + "-" + to + " " + out);
-					if (out.length() > 1) {
-						matrix[i][j] = (out.split("\n").length);
-					}
-					//System.out.println("To:" + to + "¥tFrom:" + from + "¥tCount:" + matrix[i][j]);
-					result.clear();
-
-				}
-			}
-			for (int i = 0; i < matrix.length; i++) {
-				for (int j = i+1; j < matrix[i].length; j++) {
-					int value = matrix[i][j] + matrix[j][i];
-					if (value > 0) {
-						data += node[i].split("\t")[0] + "\t" + node[j].split("\t")[0] + "\t" + String.valueOf(value) + "\n";
-					}
-				}
-			}
-			//log.info(data);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return data;
-	}
-
-	private String getUserTalkContribs(final String lang, String to, String from, final String nextId) {
-		String rvstartid = "&rvstartid=" + nextId;
-		if (nextId.equals("")) {
-			rvstartid = "";
-		}
-		String xml = "";
-		try {
-			to = URLEncoder.encode(to,"UTF-8");
-			from = URLEncoder.encode(from,"UTF-8");
-			String urlStr = "http://" + lang + ".wikipedia.org/w/api.php?format=xml&action=query&prop=revisions&titles=User_talk:" + to + "&rvlimit=500&rvprop=flags%7Ctimestamp%7Cuser&rvuser=" + from + rvstartid;
-			//log.info(urlStr);
-			URL url = new URL(urlStr);
-			HttpURLConnection urlCon = (HttpURLConnection)url.openConnection();
-			urlCon.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; ja-jp) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16");
-			urlCon.setRequestMethod("GET");
-			urlCon.setInstanceFollowRedirects(false);
-
-			urlCon.connect();
-
-
-			BufferedReader reader = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
-			String line;
-
-			while ((line = reader.readLine()) != null) {
-				//tmpData.append(line + "\n");
-				xml += line + "\n";
-			}
-			//log.info(xml);
-			reader.close();
-
-		} catch (MalformedURLException e) {
-			// ...
-			LOG.info(e.getMessage());
-		} catch (IOException e) {
-			// ...
-			LOG.info(e.getMessage());
-		}
-		return xml;
-	}
+    private String getUserTalkContribs(final String lang,
+                                       String to,
+                                       String from,
+                                       final WikiAPIClient wikiAPIClient) {
+        try {
+            to = URLEncoder.encode(to, Const.ENCODING);
+            from = URLEncoder.encode(from, Const.ENCODING);
+            String urlStr = "http://"
+                    + lang
+                    + ".wikipedia.org/w/api.php?format=xml&action=query&prop=revisions&titles=User%20talk:"
+                    + to + "&rvlimit=500&rvprop=flags%7Ctimestamp%7Cuser%7Csize&rvuser=" + from;
+            LOG.info(urlStr);
+            return wikiAPIClient.executeHTTPRequest(urlStr);
+        } catch (UnsupportedEncodingException e) {
+            LOG.error("UnsupportedEncodingException", e);
+        }
+        return "";
+    }
 }
-
