687ddfc84301bf174610053b9dc0f3531dc7d3d0
Reto Kleeb <retokl@gmail.com>
2011-05-17 11:15:53 -0400

    cleanup for matrix calculations

--- src/main/java/edu/mit/cci/visualize/wiki/collector/UsertalkNetworkFetcher.java
+++ src/main/java/edu/mit/cci/visualize/wiki/collector/UsertalkNetworkFetcher.java
@@ -18,17 +18,19 @@ import edu.mit.cci.visualize.wiki.xml.XMLTransformer;
 public class UsertalkNetworkFetcher {
 
     private static final Logger LOG = LoggerFactory.getLogger(UsertalkNetworkFetcher.class.getName());
-    private final String lang;
 
-    public UsertalkNetworkFetcher(final String lang) {
+    private final String lang;
+    private final DefaultHttpClient httpclient = new DefaultHttpClient();
+    private final WikiAPIClient wikiAPIClient = new WikiAPIClient(httpclient, false);
+    private final List<String> sanitizedUserIDs;
+
+    public UsertalkNetworkFetcher(final String lang, final List<String> userIDs) {
         this.lang = lang;
+        sanitizedUserIDs = sanitizeUserIDs(userIDs);
     }
 
-    public String getNetwork(final List<String> userIDs) {
-        List<String> sanitizedUserIDs = sanitizeUserIDs(userIDs);
-        String data = "";
-        DefaultHttpClient httpclient = new DefaultHttpClient();
-        WikiAPIClient wikiAPIClient = new WikiAPIClient(httpclient, false);
+    public List<UsertalkEdge> getNetwork() {
+        List<UsertalkEdge> userTalkEdges = Lists.newArrayList();
         try {
             int numberOfUsersInNetwork = sanitizedUserIDs.size();
             int[][] userTalkMatrix = new int[numberOfUsersInNetwork][numberOfUsersInNetwork];
@@ -38,23 +40,15 @@ public class UsertalkNetworkFetcher {
                     if (i == j) {
                         continue;
                     }
-                    String from = sanitizedUserIDs.get(i);
-                    String to = sanitizedUserIDs.get(j);
-                    String xml = getUserTalkContribs(to, from, wikiAPIClient);
-                    if (xml.indexOf("<revisions>") > 0) {
-                        Api revisionFromXML = XMLTransformer.getRevisionFromXML(xml);
-                        int numberOfRevisions = revisionFromXML.getAllRevisionsForRequest().size();
-                        if (numberOfRevisions >= 1) {
-                            userTalkMatrix[i][j] = numberOfRevisions;
-                        }
-                    }
+                    updateMatrixValues(i, j, userTalkMatrix);
                 }
             }
+
             for (int i = 0; i < userTalkMatrix.length; i++) {
                 for (int j = i + 1; j < userTalkMatrix[i].length; j++) {
                     int totalConversations = userTalkMatrix[i][j] + userTalkMatrix[j][i]; //sum of talk in both directions
                     if (totalConversations > 0) {
-                        data += sanitizedUserIDs.get(i) + "\t" + sanitizedUserIDs.get(j) + "\t" + totalConversations + "\n";
+                        userTalkEdges.add(new UsertalkEdge(sanitizedUserIDs.get(i), sanitizedUserIDs.get(j), totalConversations));
                     }
                 }
             }
@@ -63,7 +57,20 @@ public class UsertalkNetworkFetcher {
         } finally {
             httpclient.getConnectionManager().shutdown();
         }
-        return data;
+        return userTalkEdges;
+    }
+
+    private void updateMatrixValues(final int i, final int j, final int[][] userTalkMatrix) throws Exception {
+        final String from = sanitizedUserIDs.get(i);
+        final String to = sanitizedUserIDs.get(j);
+        final String xml = getUserTalkContribs(to, from, wikiAPIClient);
+        if (xml.indexOf("<revisions>") > 0) {
+            Api revisionFromXML = XMLTransformer.getRevisionFromXML(xml);
+            int numberOfRevisions = revisionFromXML.getAllRevisionsForRequest().size();
+            if (numberOfRevisions >= 1) {
+                userTalkMatrix[i][j] = numberOfRevisions;
+            }
+        }
     }
 
     private List<String> sanitizeUserIDs(final List<String> userIDs) {
@@ -80,8 +87,7 @@ public class UsertalkNetworkFetcher {
         try {
             to = URLEncoder.encode(to, Const.ENCODING);
             from = URLEncoder.encode(from, Const.ENCODING);
-            String urlStr = "http://"
-                    + lang
+            String urlStr = "http://" + lang
                     + ".wikipedia.org/w/api.php?format=xml&action=query&prop=revisions&titles=User%20talk:"
                     + to + "&rvlimit=500&rvprop=flags%7Ctimestamp%7Cuser%7Csize&rvuser=" + from;
             return wikiAPIClient.executeHTTPRequest(urlStr);
