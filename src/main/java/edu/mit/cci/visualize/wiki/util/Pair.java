58fea9b8f03819f4c30a5ddb47888ecfe032534f
Reto Kleeb <retokl@gmail.com>
2011-05-18 09:40:32 -0400

    added pair type as a key for usertalk cache elements

---
+++ src/main/java/edu/mit/cci/visualize/wiki/util/Pair.java
@@ -0,0 +1,64 @@
+package edu.mit.cci.visualize.wiki.util;
+
+import java.io.Serializable;
+
+public class Pair<T> implements Serializable{
+
+    private static final long serialVersionUID = -8829486556226525759L;
+
+    private final T first;
+    private final T second;
+
+    public Pair(final T first, final T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((first == null) ? 0 : first.hashCode());
+        result = prime * result + ((second == null) ? 0 : second.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pair<?> other = (Pair<?>) obj;
+        if (first == null) {
+            if (other.first != null)
+                return false;
+        } else if (!first.equals(other.first))
+            return false;
+        if (second == null) {
+            if (other.second != null)
+                return false;
+        } else if (!second.equals(other.second))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair [first=" + first + ", second=" + second + "]";
+    }
+
+
+
+
+}
