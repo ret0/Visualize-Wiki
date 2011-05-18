75ea6b4307bd51520cb3b1389f776aba344e1049
Reto Kleeb <retokl@gmail.com>
2011-05-18 11:30:40 -0400

    replaced 2d array with map, various cleanups

--- src/main/java/edu/mit/cci/visualize/wiki/fetcher/UsertalkNetworkFetcher.java
+++ src/main/java/edu/mit/cci/visualize/wiki/fetcher/UsertalkNetworkFetcher.java
@@ -3,6 +3,7 @@ package edu.mit.cci.visualize.wiki.fetcher;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -11,11 +12,13 @@ import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import edu.mit.cci.visualize.wiki.collector.UsertalkEdge;
 import edu.mit.cci.visualize.wiki.util.Const;
@@ -32,31 +35,31 @@ public class UsertalkNetworkFetcher {
     private final String lang;
     private final List<String> sanitizedUserIDs;
     private final int numberOfUsersInNetwork;
-    private final int[][] userTalkMatrix;
+    private final Map<Pair<String>, Integer> talkMatrix = Maps.newConcurrentMap();
     private final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(NUM_THREADS);
-    private final CacheManager manager = CacheManager.getInstance();
-    private final Cache cache = manager.getCache("usertalks");
+    private final CacheManager cacheManager = CacheManager.getInstance();
+    private final Cache userTalkCache = cacheManager.getCache("usertalks");
 
     public UsertalkNetworkFetcher(final String lang, final List<String> userIDs) {
         this.lang = lang;
         sanitizedUserIDs = sanitizeUserIDs(userIDs);
         numberOfUsersInNetwork = sanitizedUserIDs.size();
-        userTalkMatrix = new int[numberOfUsersInNetwork][numberOfUsersInNetwork];
     }
 
     public List<UsertalkEdge> getNetwork() {
-        for (int i = 0; i < numberOfUsersInNetwork; i++) {
-            for (int j = 0; j < numberOfUsersInNetwork; j++) {
-                if (i == j) {
-                    continue;
+        try{
+            for (String userID1 : sanitizedUserIDs) {
+                for (String userID2 : sanitizedUserIDs) {
+                    if (StringUtils.equals(userID1, userID2)) {
+                        continue;
+                    }
+                    newFixedThreadPool.execute(new GetUserTalk(userID1, userID2));
                 }
-                final String from = sanitizedUserIDs.get(i);
-                final String to = sanitizedUserIDs.get(j);
-                newFixedThreadPool.execute(new GetUserTalk(j, i, from, to));
             }
+        } finally {
+            shutdownThreadPool();
+            cacheManager.shutdown();
         }
-        shutdownThreadPool();
-        manager.shutdown();
         return collectUserTalkEdges();
     }
 
@@ -74,17 +77,20 @@ public class UsertalkNetworkFetcher {
 
     private List<UsertalkEdge> collectUserTalkEdges() {
         List<UsertalkEdge> userTalkEdges = Lists.newArrayList();
-        for (int i = 0; i < userTalkMatrix.length; i++) {
-            for (int j = i + 1; j < userTalkMatrix[i].length; j++) {
-                int totalConversations = userTalkMatrix[i][j] + userTalkMatrix[j][i]; //sum of talk in both directions
+        for (int i = 0; i < numberOfUsersInNetwork; i++) {
+            for (int j = i + 1; j < numberOfUsersInNetwork; j++) {
+                final String from = sanitizedUserIDs.get(i);
+                final String to = sanitizedUserIDs.get(j);
+                final Integer direction1 = talkMatrix.get(new Pair<String>(from, to));
+                final Integer direction2 = talkMatrix.get(new Pair<String>(to, from));
+                int totalConversations = direction1 + direction2; //sum of talk in both directions
                 if (totalConversations > 0) {
-                    userTalkEdges.add(new UsertalkEdge(sanitizedUserIDs.get(i), sanitizedUserIDs.get(j), totalConversations));
+                    userTalkEdges.add(new UsertalkEdge(from, to, totalConversations));
                 }
             }
         }
         return userTalkEdges;
     }
-
 
     private List<String> sanitizeUserIDs(final List<String> userIDs) {
         List<String> sanitizedUserIDs = Lists.newArrayList();
@@ -94,62 +100,56 @@ public class UsertalkNetworkFetcher {
         return sanitizedUserIDs;
     }
 
-    private String getUserTalkContribs(String to,
-                                       String from,
-                                       final WikiAPIClient wikiAPIClient) {
-        try {
-            to = URLEncoder.encode(to, Const.ENCODING);
-            from = URLEncoder.encode(from, Const.ENCODING);
-            String urlStr = "http://" + lang
-                    + ".wikipedia.org/w/api.php?format=xml&action=query&prop=revisions&titles=User%20talk:"
-                    + to + "&rvlimit=500&rvprop=flags%7Ctimestamp%7Cuser%7Csize&rvuser=" + from;
-            return wikiAPIClient.executeHTTPRequest(urlStr);
-        } catch (UnsupportedEncodingException e) {
-            LOG.error("UnsupportedEncodingException", e);
-        }
-        return "";
-    }
-
     private final class GetUserTalk implements Runnable {
-        private final String from;
-        private final String to;
-        private final int j;
-        private final int i;
 
-        private GetUserTalk(final int j, final int i, final String from, final String to) {
-            this.j = j;
-            this.i = i;
-            this.from = from;
-            this.to = to;
+        private final Pair<String> userCommunicationPair;
+
+        public GetUserTalk(final String from, final String to) {
+            userCommunicationPair = new Pair<String>(from, to);
         }
 
         @Override
         public void run() {
-
-
-            Pair<String> userCommunicationPair = new Pair<String>(from, to);
-
-            if(cache.get(userCommunicationPair) != null) {
-                userTalkMatrix[j][i] = (Integer) cache.get(userCommunicationPair).getValue();
+            final Element pairFromCache = userTalkCache.get(userCommunicationPair);
+            if(pairFromCache != null) {
+                talkMatrix.put(userCommunicationPair, (Integer) pairFromCache.getValue());
             } else {
-                final DefaultHttpClient httpclient = new DefaultHttpClient();
-                final WikiAPIClient wikiAPIClient = new WikiAPIClient(httpclient, false);
-                final String xml = getUserTalkContribs(to, from, wikiAPIClient);
-                int numberOfRevisions = 0;
-                if (xml.indexOf("<revisions>") > 0) {
-                    Api revisionFromXML;
-                    try {
-                        revisionFromXML = XMLTransformer.getRevisionFromXML(xml);
-                        numberOfRevisions = revisionFromXML.getAllRevisionsForRequest().size();
-
-                    } catch (Exception e) {
-                        LOG.error("Exception while executing Fetch Thread", e);
-                    }
-                }
-                cache.put(new Element(userCommunicationPair, numberOfRevisions));
-                cache.flush();
-                userTalkMatrix[j][i] = numberOfRevisions;
+                int numberOfRevisions = downloadPairCommunication();
+                talkMatrix.put(userCommunicationPair, numberOfRevisions);
             }
+        }
+
+        private int downloadPairCommunication() {
+            final WikiAPIClient wikiAPIClient = new WikiAPIClient(new DefaultHttpClient(), false);
+            final String xml = getUserTalkContribs(userCommunicationPair, wikiAPIClient);
+            int numberOfRevisions = 0;
+            if (xml.indexOf("<revisions>") > 0) {
+                Api revisionFromXML;
+                try {
+                    revisionFromXML = XMLTransformer.getRevisionFromXML(xml);
+                    numberOfRevisions = revisionFromXML.getAllRevisionsForRequest().size();
+                } catch (Exception e) {
+                    LOG.error("Exception while executing Fetch Thread", e);
+                }
+            }
+            userTalkCache.put(new Element(userCommunicationPair, numberOfRevisions));
+            userTalkCache.flush();
+            return numberOfRevisions;
+        }
+
+        private String getUserTalkContribs(final Pair<String> userCommunicationPair,
+                                           final WikiAPIClient wikiAPIClient) {
+            try {
+                final String from = URLEncoder.encode(userCommunicationPair.getFirst(), Const.ENCODING);
+                final String to = URLEncoder.encode(userCommunicationPair.getSecond(), Const.ENCODING);
+                String urlStr = "http://" + lang
+                        + ".wikipedia.org/w/api.php?format=xml&action=query&prop=revisions&titles=User%20talk:"
+                        + to + "&rvlimit=500&rvprop=flags%7Ctimestamp%7Cuser%7Csize&rvuser=" + from;
+                return wikiAPIClient.executeHTTPRequest(urlStr);
+            } catch (UnsupportedEncodingException e) {
+                LOG.error("UnsupportedEncodingException", e);
+            }
+            return "";
         }
     }
 }
